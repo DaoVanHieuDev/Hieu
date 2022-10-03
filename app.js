eeb8154a0c718a02d71015dcc0e4d4d82e508a36
DaoVanHieuDev <daovanhieu204@gmail.com>
2022-10-03 12:44:28 +0700

    minitest

---
+++ app.js
@@ -0,0 +1,43 @@
+// //////===========================BÀi 1================////
+// let b = "Moi ban nhap vao";
+// let newb = ("");
+// for (let i = b.length - 1;i >=0;i --) {
+//     newb += b[i];
+// }
+// console.log(newb);
+
+
+////==================BAI 2===================////
+// let d = prompt("MOi ban nhap vao mot chuoi bat ki");
+// let sent = d.toLocaleLowerCase().split(" ");
+// for(var i = 0;i<sent.length;i++) {
+//     sent[i]= sent[i][0].toUpperCase()+sent[i].slice(1);
+// }
+// console.log(sent.join(" "));    
+
+/// ======================== bai 5==============/////
+// let names = {
+//     name1: "Hoa",
+//     name2:"Huyen",
+//     name3:"Hue"
+// }
+// names.file = " nhan vien";
+//delete names.file
+// console.log(names.name1);
+// console.log(names);
+
+// ================ bai 3=============//
+// let tiger = prompt("Moi ban nhap vao mot mang bat ki");
+
+// let array = [1,3,3,4,5,5,7,8,9];
+// let result = [];
+// result = array.filter(function(element){
+//   return result.includes(element) ? '' : result.push(element)
+// });
+
+// console.log(result);
+//[ 1, 2, 4, 3, 76 ]
+
+
+// ===============BAI4==========//
+let ace=prompt("Moi ban  nhap vao mot mang bat ki");
